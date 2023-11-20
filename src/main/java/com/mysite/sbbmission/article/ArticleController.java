f5e38cb46735e5119527f51c2add0e336bce77b0
Bisi3asi <kueeng8008@naver.com>
2023-11-21 02:09:40 +0900

    feat : showDetail

--- src/main/java/com/mysite/sbbmission/article/ArticleController.java
+++ src/main/java/com/mysite/sbbmission/article/ArticleController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,5 +19,12 @@ public class ArticleController {
         List<Article> articleList = articleService.getList();
         model.addAttribute("articleList", articleList);
         return "article_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String showDetail(Model model, @PathVariable("id") Long id){
+        Article article = articleService.getArticle(id);
+        model.addAttribute("article", article);
+        return "question_detail";
     }
 }
