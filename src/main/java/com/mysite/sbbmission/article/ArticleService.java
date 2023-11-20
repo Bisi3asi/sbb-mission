5609f6e9effec8e352a33321e73d4cb4fefac37d
Bisi3asi <kueeng8008@naver.com>
2023-11-21 02:06:54 +0900

    feat : getArticle

--- src/main/java/com/mysite/sbbmission/article/ArticleService.java
+++ src/main/java/com/mysite/sbbmission/article/ArticleService.java
@@ -1,9 +1,11 @@
 package com.mysite.sbbmission.article;
 
+import com.mysite.sbbmission.global.exceptions.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +14,16 @@ public class ArticleService {
 
     public List<Article> getList() {
         return this.articleRepository.findAll();
+    }
+
+    public Article getArticle(Long id){
+        Optional<Article> article = this.articleRepository.findById(id);
+
+        if (article.isPresent()){
+            return article.get();
+        }
+        else {
+            throw new DataNotFoundException("존재하지 않는 게시물입니다");
+        }
     }
 }
