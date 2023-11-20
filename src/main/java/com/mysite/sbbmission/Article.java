982073b35c06cb4406ac5ddbae03042998839ddb
Bisi3asi <kueeng8008@naver.com>
2023-11-20 18:00:49 +0900

    feat : Article(entity)

---
+++ src/main/java/com/mysite/sbbmission/Article.java
@@ -0,0 +1,30 @@
+package com.mysite.sbbmission;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(columnDefinition = "VARCHAR(50)", length = 50)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT", length = 500)
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+}
