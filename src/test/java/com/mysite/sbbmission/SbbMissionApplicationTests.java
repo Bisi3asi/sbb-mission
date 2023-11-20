82bc5f3c8853485b110deb96df3579a5b7179578
Bisi3asi <kueeng8008@naver.com>
2023-11-20 23:21:30 +0900

    test : remove id value in case1
    * GenerationType.IDENTITY

--- src/test/java/com/mysite/sbbmission/SbbMissionApplicationTests.java
+++ src/test/java/com/mysite/sbbmission/SbbMissionApplicationTests.java
@@ -18,7 +18,6 @@ class SbbMissionApplicationTests {
     void 글_추가() {
         // case 1
         Article a1 = Article.builder()
-                .id(1L)
                 .subject("글 1")
                 .content("내용 1")
                 .createDateTime(LocalDateTime.MAX)
@@ -27,7 +26,6 @@ class SbbMissionApplicationTests {
 
         // case 2
         Article a2 = Article.builder()
-                .id(2L)
                 .subject("글 2")
                 .content("내용 2")
                 .createDateTime(LocalDateTime.MAX)
@@ -36,9 +34,8 @@ class SbbMissionApplicationTests {
 
         // case 3
         Article a3 = Article.builder()
-                .id(2L)
-                .subject("글 2")
-                .content("내용 2")
+                .subject("글 3")
+                .content("내용 3")
                 .createDateTime(LocalDateTime.MAX)
                 .modifiedDateTime(LocalDateTime.MAX)
                 .build();
@@ -48,5 +45,4 @@ class SbbMissionApplicationTests {
         articleRepository.save(a2);
         articleRepository.save(a3);
     }
-
 }
