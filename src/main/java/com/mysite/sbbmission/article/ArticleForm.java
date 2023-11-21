2f25aa836bed3858b26c5e9c3af0c631ce381b5f
Bisi3asi <kueeng8008@naver.com>
2023-11-22 02:45:51 +0900

    feat(ArticleForm) : error message(validation)

--- src/main/java/com/mysite/sbbmission/article/ArticleForm.java
+++ src/main/java/com/mysite/sbbmission/article/ArticleForm.java
@@ -2,17 +2,17 @@ package com.mysite.sbbmission.article;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
+@Setter
 public class ArticleForm {
-    @NotBlank(message="제목은 필수항목입니다.")
-    @Size(max = 50, message = "제목은 50자를 초과할 수 없습니다.")
+    @NotBlank(message = "글 제목을 작성해주세요.")
+    @Size(max = 50, message = "50자 미만으로 작성해주세요!")
     private String title;
 
-    @NotBlank(message="내용은 필수항목입니다.")
-    @Size(max = 500, message = "내용은 500자를 초과할 수 없습니다.")
+    @NotBlank(message = "글 내용을 작성해주세요.")
+    @Size(max = 500, message = "500자 미만으로 작성해주세요!")
     private String content;
 }
