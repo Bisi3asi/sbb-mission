8fdb9ba06335a22dfb97e6c3bd030c87deba1713
Bisi3asi <kueeng8008@naver.com>
2023-11-21 10:37:30 +0900

    feat : CommentController

---
+++ src/main/java/com/mysite/sbbmission/comment/CommentController.java
@@ -0,0 +1,27 @@
+package com.mysite.sbbmission.comment;
+
+import com.mysite.sbbmission.article.Article;
+import com.mysite.sbbmission.article.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+    private final CommentService commentService;
+    private final ArticleService articleService;
+
+    @PostMapping("/create/{id}")
+    public String post(Model model, @PathVariable("id") Long id, @RequestParam String content){
+        Article article = articleService.getArticle(id);
+        commentService.create(article, content);
+        model.addAttribute("article", article);
+        return String.format("redirect:/comment/detail/%s", id);
+    }
+}
