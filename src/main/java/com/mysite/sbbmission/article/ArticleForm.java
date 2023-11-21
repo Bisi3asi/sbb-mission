1ebff872352dc52a8aaf7e2a39288c81298801c7
Bisi3asi <kueeng8008@naver.com>
2023-11-21 23:16:59 +0900

    fix(ArticleForm) : NotEmpty to NotBlank

--- src/main/java/com/mysite/sbbmission/article/ArticleForm.java
+++ src/main/java/com/mysite/sbbmission/article/ArticleForm.java
@@ -1,6 +1,6 @@
 package com.mysite.sbbmission.article;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,11 +10,11 @@ import lombok.Getter;
 @Builder
 @Getter
 public class ArticleForm {
-    @NotEmpty(message="제목은 필수항목입니다.")
+    @NotBlank(message="제목은 필수항목입니다.")
     @Size(max = 50, message = "제목은 50자를 초과할 수 없습니다.")
     private String title;
 
-    @NotEmpty(message="내용은 필수항목입니다.")
+    @NotBlank(message="내용은 필수항목입니다.")
     @Size(max = 500, message = "내용은 500자를 초과할 수 없습니다.")
     private String content;
 }
