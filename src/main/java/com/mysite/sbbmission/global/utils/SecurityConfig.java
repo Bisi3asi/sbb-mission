29c6a93081e0db5c0ab6729a525aaad57a000f3b
Bisi3asi <kueeng8008@naver.com>
2023-11-22 17:21:39 +0900

    feat : SecurityConfig

---
+++ src/main/java/com/mysite/sbbmission/global/utils/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.mysite.sbbmission.global.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                // 모든 경로에 대해 인증되지 않은 요청을 승인(개발용)
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                // h2-console csrf 검증 예외 처리
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                // h2-console X-Frame Option 헤더값 방지
+                .headers(headers -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+        return http.build();
+    }
+}
