4ab0bf698d9f659f5e6bd0d7708a894ddcf5538e
Bisi3asi <kueeng8008@naver.com>
2023-11-23 02:19:39 +0900

    feat : MemberRepository

---
+++ src/main/java/com/mysite/sbbmission/member/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.mysite.sbbmission.member;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
