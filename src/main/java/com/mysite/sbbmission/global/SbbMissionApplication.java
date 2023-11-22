505f7e653565cb335a240626b0e791c2b96e74fa
Bisi3asi <kueeng8008@naver.com>
2023-11-23 02:18:59 +0900

    refactor : move dir to global

---
+++ src/main/java/com/mysite/sbbmission/global/SbbMissionApplication.java
@@ -0,0 +1,12 @@
+package com.mysite.sbbmission.global;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbMissionApplication.class, args);
+    }
+}
