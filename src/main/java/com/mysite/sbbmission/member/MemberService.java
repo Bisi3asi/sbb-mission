75278e10707588424eb4acc84f0da2b4d42bf7e4
Bisi3asi <kueeng8008@naver.com>
2023-11-24 00:15:06 +0900

    fix(MemberService) : add validation (if attribute exists)

--- src/main/java/com/mysite/sbbmission/member/MemberService.java
+++ src/main/java/com/mysite/sbbmission/member/MemberService.java
@@ -11,22 +11,23 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public Member create(String signInId, String username, String email, String password) {
-        if (memberRepository.findBySignInId(signInId).isPresent()){
-            throw new DataIntegrityViolationException(MemberUtilConst.SIGNIN_ID.UNUNIQUE_MESSAGE);
-        }
-        if (memberRepository.findByUsername(username).isPresent()){
-            throw new DataIntegrityViolationException(MemberUtilConst.USERNAME.UNUNIQUE_MESSAGE);
-        }
-        if (memberRepository.findByEmail(email).isPresent()){
-            throw new DataIntegrityViolationException(MemberUtilConst.EMAIL.UNUNIQUE_MESSAGE);
-        }
+    public Member create(MemberSignUpForm memberSignUpForm) {
+        StringBuilder exceptionBuilder = new StringBuilder();
+        if (memberRepository.findBySignInId(memberSignUpForm.getSignInId()).isPresent())
+            exceptionBuilder.append(MemberUtilConst.SIGNIN_ID.UNUNIQUE_MESSAGE);
+        if (memberRepository.findByUsername(memberSignUpForm.getUsername()).isPresent())
+            exceptionBuilder.append(MemberUtilConst.USERNAME.UNUNIQUE_MESSAGE);
+        if (memberRepository.findByEmail(memberSignUpForm.getEmail()).isPresent())
+            exceptionBuilder.append(MemberUtilConst.EMAIL.UNUNIQUE_MESSAGE);
+
+        if (!exceptionBuilder.isEmpty())
+            throw new DataIntegrityViolationException(exceptionBuilder.toString());
 
         Member member = Member.builder()
-                .signInId(signInId)
-                .username(username)
-                .email(email)
-                .password(passwordEncoder.encode(password))
+                .signInId(memberSignUpForm.getSignInId())
+                .username(memberSignUpForm.getUsername())
+                .email(memberSignUpForm.getEmail())
+                .password(passwordEncoder.encode(memberSignUpForm.getPassword1()))
                 .build();
 
         memberRepository.save(member);
