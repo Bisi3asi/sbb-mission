d9ea0f150b95a6789ed29ed3c6c7c994468d6454
Bisi3asi <kueeng8008@naver.com>
2023-11-25 03:04:19 +0900

    feat : CommentService(update)

--- src/main/java/com/mysite/sbbmission/comment/CommentService.java
+++ src/main/java/com/mysite/sbbmission/comment/CommentService.java
@@ -1,12 +1,14 @@
 package com.mysite.sbbmission.comment;
 
 import com.mysite.sbbmission.article.Article;
+import com.mysite.sbbmission.global.exceptions.DataNotFoundException;
 import com.mysite.sbbmission.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +23,21 @@ public class CommentService {
                 .createDateTime(LocalDateTime.now())
                 .article(article)
                 .author(author)
+                .build();
+        commentRepository.save(comment);
+    }
+
+    public Comment getComment(long id){
+        Optional<Comment> comment = this.commentRepository.findById(id);
+        if (comment.isPresent()){
+            return comment.get();
+        }
+        else throw new DataNotFoundException("comment not found");
+    }
+
+    public void update(Comment comment, String content){
+        comment.toBuilder()
+                .content(content)
                 .build();
         commentRepository.save(comment);
     }
