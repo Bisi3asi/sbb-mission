c6a88d6d742f92c3d4a804efdba8cd6d1c0ef4df
Bisi3asi <kueeng8008@naver.com>
2023-11-28 01:40:37 +0900

    feat(Comment) : anchor

--- src/main/java/com/mysite/sbbmission/comment/CommentController.java
+++ src/main/java/com/mysite/sbbmission/comment/CommentController.java
@@ -51,6 +51,7 @@ public class CommentController {
         if (brs.hasErrors()){
             // 에러 발생 시 article을 model로 다시 실어보낸다
             model.addAttribute("article", comment.getArticle());
+            // todo : 해당 부분 오류 해결(추천 / 비추천 model attribute 추가 후 발생)
             return "article/article_detail";
         }
         commentService.update(comment, commentForm);
