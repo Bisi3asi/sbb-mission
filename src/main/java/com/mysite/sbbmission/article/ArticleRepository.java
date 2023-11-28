6d9c98ca40e6e6ff3300ee3c2f41d8f7b2530b1d
Bisi3asi <kueeng8008@naver.com>
2023-11-28 18:14:35 +0900

    feat(articleRepository) : add JPA Specification

--- src/main/java/com/mysite/sbbmission/article/ArticleRepository.java
+++ src/main/java/com/mysite/sbbmission/article/ArticleRepository.java
@@ -1,7 +1,12 @@
 package com.mysite.sbbmission.article;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     // JpaRepository<Entity T, PK T>
+    Page<Article> findAll(Pageable pageable);
+    Page<Article> findAll(Specification<Article> spec, Pageable pageable);
 }
